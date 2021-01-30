4b6c232c29d1e7e8b7825bd35b2bbd71faed7b54
Magnus Teschner <magnus.2002.mt@gmail.com>
2021-01-30 22:53:47 +0100

    Running

--- main.py
+++ main.py
@@ -3,5 +3,16 @@ import pygame
 #Initialize pygame
 pygame.init()
 
-screen = pygame.display.set_mode(())
+#Create the Screen
+screen = pygame.display.set_mode((800, 600))
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+
 
